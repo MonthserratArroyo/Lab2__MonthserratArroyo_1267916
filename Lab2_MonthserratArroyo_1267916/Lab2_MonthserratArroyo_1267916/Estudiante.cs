929a6b9eb1cd6992b04e6b910f43ac3a14cf413a
MonthserratArroyo <30309223+MonthserratArroyo@users.noreply.github.com>
2017-09-01 07:59:58 -0600

    Add files via upload

---
+++ Lab2_MonthserratArroyo_1267916/Lab2_MonthserratArroyo_1267916/Estudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_MonthserratArroyo_1267916
+{
+    /// <summary>
+    /// Clase estudiante para el laboratorio número 2
+    /// </summary>
+    public class Estudiante
+    {
+
+        public string NombreEstudiante { get; set; }
+        public bool GeneroEstudiante { get; set; }
+   /// <summary>
+   /// Constructor de la clase estudiante
+   /// </summary>
+   /// <param name="nombre">Nombre del estudiante</param>
+   /// <param name="genero">Genero del estudiante, false para hombre, true para mujer</param>
+        public Estudiante(string nombre, bool genero)
+        {
+            
+            NombreEstudiante = nombre;
+            GeneroEstudiante = genero;
+            
+            
+        }
+    }
+}
---
+++ Lab2_MonthserratArroyo_1267916/Lab2_MonthserratArroyo_1267916/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_MonthserratArroyo_1267916
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Estudiante estudiante = new Estudiante("Montse", true);
+
+            //Lista de enteros
+            List<int> ListaDeEnteros = new List<int>();
+            //Pila de enteros
+            Stack<int> PilaDeEnteros = new Stack<int>();
+            //Cola de enteros
+            Queue<int> ColaDeEnteros = new Queue<int>();
+
+            //AGREGAR ITEMS LISTA
+            ListaDeEnteros.Add(150);
+            ListaDeEnteros.Add(740);
+            ListaDeEnteros.Add(660);
+            ListaDeEnteros.Add(100);
+            ListaDeEnteros.Add(75);
+            //AGREGAR ITEMS PILA
+            PilaDeEnteros.Push(255);
+            //AGREGAR ITEMS COLA
+            ColaDeEnteros.Enqueue(75);
+
+
+            //REMOVER ITEMS DE LISTA
+            ListaDeEnteros.RemoveAt(0);
+            //REMOVER ITEMS DE PILA
+            PilaDeEnteros.Pop();
+            //REMOVER ITEMS DE COLA
+            ColaDeEnteros.Dequeue();
+
+            foreach (int entero in ListaDeEnteros)
+            {
+                //hacer algo
+                MessageBox.Show(entero.ToString());
+            }
+
+        
+        }
+    }
+}
